3bb707a8398e664771b16dd398146fe49327333f
thenameisvicky <vigneshbalasubramaniyan3@gmail.com>
2025-05-16 00:58:07 +0530

    "May  16 2025 Search Insert Position Problem"

---
+++ Arrays/complexity Optimized/findInsertPosition.js
@@ -0,0 +1,23 @@
+//@Description: Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// @Time Complexity: O(log n)
+
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    //Mid is nessesary always in binary search
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  //Returning left because it will be the smaller index where element would need to be placed!
+  return left;
+};
+
+console.log(`Find Insert Position: ${searchInsert([1,3,5,6],5)}`);
