212281818b1c76676344d55de15088bb70b79786
thenameisvicky <vigneshbalasubramaniyan3@gmail.com>
2025-05-31 21:49:51 +0530

    "May 31 2025 - Added Explanation - maximumSumSubarray.js"

--- Arrays/mySolutions/maximumSumSubarray.js
+++ Arrays/mySolutions/maximumSumSubarray.js
@@ -2,15 +2,22 @@
 //@Pattern: Kdane's Algorithum (sliding window)
 
 var maxSumSubarray = function (nums) {
+  //Sarting currentsum from 0 cause we dont know the max sum
   let currentSum = 0;
+  //Having max as -Infinity cause currently there is no element left in array to compare
   let max = -Infinity;
+  //Iterating through the array
   for (let i = 0; i < nums.length; i++) {
+    //Increasing the sum
     currentSum += nums[i];
+    //Calculating the Max of max and currentsum
     max = Math.max(max, currentSum);
     if (currentSum < 0) {
+      //Edge case if currentsum < 0 then we set it as current sum
       currentSum = 0;
     }
   }
+  //Finally returning max
   return max;
 };
 
