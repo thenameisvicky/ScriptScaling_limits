bff5502cfbbaf2e7dcad4e8a8a4a08e86e7754b4
thenameisvicky <vigneshbalasubramaniyan3@gmail.com>
2025-06-15 10:54:49 +0530

    "Jun 15 New Problem solved mergeSortedArray.js"

---
+++ Arrays/mySolutions/mergeSortedArray.js
@@ -0,0 +1,28 @@
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
+};
+
+let nums1 = [1, 2, 3, 0, 0, 0],
+  m = 3,
+  nums2 = [2, 5, 6],
+  n = 3;
+
+merge(nums1, m, nums2, n);
+console.log("Merge Sorted Array In-Place:", nums1);
